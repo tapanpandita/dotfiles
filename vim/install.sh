c10c4cddd74b5d84fe10b8f37b1659c7da94ac9a
Tapan Pandita <tapan.pandita@gmail.com>
2014-04-20 03:37:48 +0530

    create vim dirs

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,12 @@
 CURRENT_DIR=pwd
-VIM_COLORS_DIR=$HOME/.vim/colors
-VUNDLE_DIR=$HOME/.vim/bundle/vundle
+VIM_BASE_DIR=$HOME/.vim
+VIM_COLORS_DIR=$VIM_BASE_DIR/colors
+VUNDLE_DIR=$VIM_BASE_DIR/bundle/vundle
+VIM_TMP_DIR=$VIM_BASE_DIR/tmp
+VIM_BACKUP_DIR=$VIM_BASE_DIR/backup
+
+mkdir -p $VIM_TMP_DIR
+mkdir -p $VIM_BACKUP_DIR
 
 mkdir -p $VIM_COLORS_DIR
 wget http://www.vim.org/scripts/download_script.php\?src_id\=15762 -O $VIM_COLORS_DIR/xoria256.vim
