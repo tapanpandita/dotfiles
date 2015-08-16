250d0f21aec55e0ff565958bc14e19ca13182e8a
Tapan Pandita <tapan.pandita@gmail.com>
2015-08-17 01:20:14 +0530

    fixed vundle install command

--- vim/install.sh
+++ vim/install.sh
@@ -1,7 +1,6 @@
 CURRENT_DIR=pwd
 VIM_BASE_DIR=$HOME/.vim
 VIM_COLORS_DIR=$VIM_BASE_DIR/colors
-VUNDLE_DIR=$VIM_BASE_DIR/bundle/vundle
 VIM_TMP_DIR=$VIM_BASE_DIR/tmp
 VIM_BACKUP_DIR=$VIM_BASE_DIR/backup
 
@@ -11,7 +10,7 @@ mkdir -p $VIM_BACKUP_DIR
 mkdir -p $VIM_COLORS_DIR
 wget http://www.vim.org/scripts/download_script.php\?src_id\=15762 -O $VIM_COLORS_DIR/xoria256.vim
 
-git clone https://github.com/gmarik/vundle.git $VUNDLE_DIR
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 cd ~/.vim/bundle/YouCompleteMe
